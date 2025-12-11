5c4c4bccad24b9260e51718f07ba26cf1198e874
abdirahmanmahamoud <abdirahmanmahamoud14@gmail.com>
2025-12-11 20:03:39 +0300

    featRefactor: api folder

---
+++ api/youtube.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from "express";
+
+export const getYouTubeChannels = (req: Request, res: Response) => {
+  try {
+    // get youtube channels from youtube api
+    const youtubeChannels: any = [];
+    return res.status(200).json(youtubeChannels);
+  } catch (error: any) {
+    return res.status(500).json({
+      message: error.message,
+    });
+  }
+};
