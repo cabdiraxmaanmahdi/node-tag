79f2ea6ed897b6c9fb82f266360f56f5d7de5c04
cabdiraxmaanmahdi <armohamouddev@gmail.com>
2025-12-30 15:56:41 +0300

    feat: Add sample YouTube channel data to getYouTubeChannels function

--- api/youtube.ts
+++ api/youtube.ts
@@ -3,7 +3,13 @@ import { Request, Response } from "express";
 export const getYouTubeChannels = (req: Request, res: Response) => {
   try {
     // get youtube channels from youtube api
-    const youtubeChannels: any = [];
+    const youtubeChannels: any = [
+      {
+        name: "HAJI ALI MEDIA",
+        channelHandle: "@hajialimedia",
+        subscribers: "2.69k",
+      },
+    ];
     return res.status(200).json(youtubeChannels);
   } catch (error: any) {
     return res.status(500).json({
