125fc07de0506bf0fa578d88a0bbb447df1285c5
cabdiraxmaanmahdi <armohamouddev@gmail.com>
2025-12-30 15:40:02 +0300

    feat: populate getYouTubeChannels with sample data

--- api/youtube.ts
+++ api/youtube.ts
@@ -3,7 +3,18 @@ import { Request, Response } from "express";
 export const getYouTubeChannels = (req: Request, res: Response) => {
   try {
     // get youtube channels from youtube api
-    const youtubeChannels: any = [];
+    const youtubeChannels: any = [
+      {
+        name: "HAJI ALI MEDIA",
+        channelHandle: "@hajialimedia",
+        subscribers: "2.69k",
+      },
+      {
+        name: "bein sports",
+        channelHandle: "@beinsports",
+        subscribers: "15.6m",
+      },
+    ];
     return res.status(200).json(youtubeChannels);
   } catch (error: any) {
     return res.status(500).json({
