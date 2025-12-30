1e02c1d2528da810eece4cb74e3c4da2a0a9d0e6
abdirahmanmahamoud <abdirahmanmahamoud14@gmail.com>
2025-12-30 16:22:27 +0300

    feat: Add AFCON endpoint to provide information about the tournament and participating countries

--- index.ts
+++ index.ts
@@ -11,6 +11,67 @@ app.get("/", (req, res) => {
   res.send("Hello World on port 9000");
 });
 
+app.get("afcom", (req, res) => {
+  res.json({
+    message: "AFCON is the biggest football tournament in Africa",
+    countries: [
+      "Algeria",
+      "Angola",
+      "Benin",
+      "Botswana",
+      "Burkina Faso",
+      "Burundi",
+      "Cameroon",
+      "Cape Verde",
+      "Central African Republic",
+      "Chad",
+      "Comoros",
+      "Democratic Republic of the Congo",
+      "Djibouti",
+      "Egypt",
+      "Equatorial Guinea",
+      "Eritrea",
+      "Eswatini",
+      "Ethiopia",
+      "Gabon",
+      "Gambia",
+      "Ghana",
+      "Guinea",
+      "Guinea-Bissau",
+      "Ivory Coast",
+      "Kenya",
+      "Lesotho",
+      "Liberia",
+      "Libya",
+      "Madagascar",
+      "Malawi",
+      "Mali",
+      "Mauritania",
+      "Mauritius",
+      "Morocco",
+      "Mozambique",
+      "Namibia",
+      "Niger",
+      "Nigeria",
+      "Rwanda",
+      "Sao Tome and Principe",
+      "Senegal",
+      "Seychelles",
+      "Sierra Leone",
+      "Somalia",
+      "South Africa",
+      "South Sudan",
+      "Sudan",
+      "Tanzania",
+      "Togo",
+      "Tunisia",
+      "Uganda",
+      "Zambia",
+      "Zimbabwe",
+    ],
+  });
+});
+
 app.get("/pl-teams", getPLTeams);
 
 app.get("/youtube", (req, res) => {
